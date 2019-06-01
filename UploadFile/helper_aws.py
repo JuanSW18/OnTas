4331a4a5a5dcc70899aa31ac0139b139cea807a1
Claara <clara.081094@gmail.com>
2019-06-01 14:09:44 -0500

    fead: refactor rename file

---
+++ UploadFile/helper_aws.py
@@ -0,0 +1,36 @@
+import boto3
+
+class Rekognition(object):
+    
+
+    def __init__(self):
+        self.collectionId = "lost"
+        self.bucket_name = "hackathon.faces"
+
+    def index_faces(self,bytess,idImage):
+
+        client = boto3.client('rekognition')
+        response = client.index_faces(
+            Image={'Bytes': bytess}, CollectionId=self.collectionId, ExternalImageId=idImage)
+
+        return response
+    
+    def upload_file_to_s3(self,archive, name,acl="public-read"):
+
+        client = boto3.client('s3')
+        try:
+            client.upload_fileobj(
+                archive,
+                self.bucket_name,
+                name,
+                ExtraArgs={
+                    "ACL": acl,
+                    "ContentType": archive.content_type
+                }
+            )
+
+        except Exception as e:
+            # This is a catch all exception, edit this part to fit your needs.
+            print("Something Happened: ", e)
+            return e
+
