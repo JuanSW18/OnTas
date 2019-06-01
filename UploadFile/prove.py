051b089eeae4bcd947fc6593a243acacf349391e
Danny Chucos <fchucoss14@gmail.com>
2019-06-01 13:07:02 -0500

    chore: testing curl

--- UploadFile/prove.py
+++ UploadFile/prove.py
@@ -1,10 +1,12 @@
-import uuid
-import base64
+import uuid, base64
 from flask import Flask, jsonify
 from flask import request
 from werkzeug.utils import secure_filename
 
+from config import Config
+
 app = Flask(__name__)
+app.config.from_object("Config")
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -12,16 +14,19 @@ def upload_file():
 		generated_file_name=str(uuid.uuid4())
 		file = request.files['data']
 		imageBase64 = base64.b64encode(file.read())
-		with open("./image64.txt", "wb") as f:
+		'''with open("./image64.txt", "wb") as f:
 			f.write(imageBase64)
-		return "hi :)"
+		return "hi :)"'''
 		# image_bytes = base64.b64decode(bytes(file))
 		# print(image_bytes)
 		#file.save('/home/danny/' + secure_filename(generated_file_name))
 		# return jsonify({ 'url': generated_file_name })
 
+		#testing: curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@debian-rules.png" http://127.0.0.1:8080/upload
+
+
 @app.route("/hola")
 def index():
     return "Hola mundo"
 
-app.run(debug=False, port=8080)
+app.run()
